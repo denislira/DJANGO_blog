b820e022a00064058450407d185c268d6ae79b70
denislira <denislira@live.com>
2020-10-10 13:31:23 -0300

    Fazendo Views

--- meu_site/blog/views.py
+++ meu_site/blog/views.py
@@ -7,4 +7,5 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
