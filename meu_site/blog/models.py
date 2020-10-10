fde1a1204c9c04c77d569e4afbf512ee9e1ce536
denislira <denislira@live.com>
2020-10-10 18:15:09 -0300

    Criamos models

--- meu_site/blog/models.py
+++ meu_site/blog/models.py
@@ -5,7 +5,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post (models.Model):
+    STATUS = (
+        ('draft','draft'), #rascunho
+        ('published','published'), #publicado
+    )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     content = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True) #salvar data 1 vez quando criar
+    Changed = models.DateTimeField(auto_now=True) #salvar data todo vez que alterar
+    status = models.CharField(max_length=10, choices=STATUS, default='rascunho')
