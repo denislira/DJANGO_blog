0476103ca7ea3328150f54ecdfec8fbdb1f6983a
denislira <denislira@live.com>
2020-10-10 14:40:26 -0300

    Criamos models

--- meu_site/blog/views.py
+++ meu_site/blog/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
+
