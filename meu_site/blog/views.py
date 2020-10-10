12e6f7398e36f75cc5e4769d5ede0643834ea232
denislira <denislira@live.com>
2020-10-10 13:13:08 -0300

    Views

--- meu_site/blog/views.py
+++ meu_site/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User)
